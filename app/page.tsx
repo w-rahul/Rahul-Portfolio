e64f46ee16d2805a73719515e04c0eac07333c75
Rahul <rahulguptaji1019@gmail.com>
2024-09-09 00:56:52 +0530

    Added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Landing } from "@/components/Landing";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-   <div>
-        Starting of the portfolio    
-   </div>
-  );
+    <div className="text-white w-full h-screen bg-black bg-[linear-gradient(to_right,rgba(255,255,255,0.1)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.1)_1px,transparent_1px)] bg-[size:40px_40px]">
+    <div>
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <Landing />
+    </div>
+    </div>
+  </div>
+  )
 }
