662232358770a4ae2cd6234ad01df1b228b7bd04
Rahul <rahulguptaji1019@gmail.com>
2024-09-09 00:55:53 +0530

    Added Landing Page

---
+++ components/Landing.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import TypingText from "./TypingText"
+
+export const Landing = () =>{
+    return <div className="flex flex-col justify-center h-screen text-center">
+        <div className="flex justify-center">
+             <div>
+                <div className="text-3xl">
+                    Hello, I am
+                </div>
+                <div className="text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600"> 
+                    Rahul Gupta 
+                </div>
+                <div className="">
+                    <TypingText />
+                </div>
+             </div>
+        </div>
+    </div>
+}
