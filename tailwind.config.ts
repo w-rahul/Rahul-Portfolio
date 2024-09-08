aff06ee4d703d91dac62b68a529d06d490bee7a4
Rahul <rahulguptaji1019@gmail.com>
2024-09-09 00:54:34 +0530

    Added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+          sharp : ["Open Sans", "sans-serif"]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
