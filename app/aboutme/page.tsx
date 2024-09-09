35e432c8fb8b96ca50f379354954a8e7fe1d6bcd
w-rahul <143905097+w-rahul@users.noreply.github.com>
2024-09-10 03:12:05 +0530

    Update page.tsx

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
         </div>
         <div className="w-full max-w-4xl flex flex-col md:flex-row justify-center border rounded-3xl mx-auto px-4 py-6 sm:px-6 lg:px-10">
           <div className="text-base sm:text-lg md:text-xl lg:text-2xl font-light">
-            <p className="pb-4">Hi! I am Rahul Gupta, a CS graduate. I currently live in America and was raised here as well.</p>
+            <p className="pb-4">Hi! I am Rahul Gupta, a CS graduate. I currently live in Delhi, India and was raised here as well.</p>
             <p className="pb-4">While being comfortable with frontend work, my real passion lies in backend development. I love creating and improving the systems that drive applications from behind the scenes.</p>
             <p className="pb-4">Currently, looking for new opportunities to dive deeper into Full-stack/backend projects and collaborate on exciting challenges.</p>
             <p className="pb-4">If you are interested in working together or just want to chat about tech, feel free to reach out!</p>
