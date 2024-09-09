343805ba77f9547dde7cbb8394ac72fdfae8d057
Rahul <rahulguptaji1019@gmail.com>
2024-09-10 00:37:55 +0530

    Updated  Skills Data

--- components/SkillData.tsx
+++ components/SkillData.tsx
@@ -1,5 +1,5 @@
 
-export interface Skill {
+export interface SkillProp {
     name : string
     logo : string
 }
@@ -13,37 +13,37 @@ export const SkillGroups : SkillGroup[] = [
     {
         heading : "Languages",
         Skills : [
-            {name : "JavaScript" , logo : "./components/assests/Logo-icons/javascript-js-seeklogo.svg"},
-            {name : "TypeScript" , logo : "./components/assests/Logo-icons/typescript-seeklogo.svg"},
-            {name : "C++" , logo : "./components/assests/Logo-icons/c-seeklogo.svg"}
+            {name : "JavaScript" , logo : "/ICONS/javascript.svg"},
+            {name : "TypeScript" , logo : "/ICONS/typescript.svg"},
+            {name : "C++" , logo : "/ICONS/cplusplus.svg"}
         ]
     },
     {
         heading : "Libraries and frameworks",
         Skills : [
-            {name : "express", logo : "./components/assests/Logo-icons/icons8-express-js.svg"},
-            {name : "Prisma", logo : "./components/assests/Logo-icons/prisma-seeklogo.svg"},
-            {name : "Zod", logo : "./components/assests/Logo-icons/zod-seeklogo.svg"},
-            {name : "react", logo : "./components/assests/Logo-icons/react-seeklogo.svg"},
-            {name : "tailwindcss", logo : "./components/assests/Logo-icons/tailwind-css-seeklogo.svg"},
-            {name : "nextjs", logo : "./components/assests/Logo-icons/next-js-seeklogo.svg"},
+            {name : "express", logo : "/ICONS/express.svg"},
+            {name : "Prisma", logo : "/ICONS/prisma.svg"},
+            {name : "Zod", logo : "/ICONS/zod.svg"},
+            {name : "react", logo : "/ICONS/react.svg"},
+            {name : "tailwindcss", logo : "/ICONS/tailwindcss.svg"},
+            {name : "nextjs", logo : "/ICONS/nextdotjs.svg"},
         ]
     },
     {
         heading : "Databases",
         Skills : [
-            {name : "Postgres", logo : "./components/assests/Logo-icons/postqresql-seeklogo.svg"},
-            {name : "Mongodb", logo : "./components/assests/Logo-icons/mongodb-seeklogo.svg"}
+            {name : "Postgres", logo : "/ICONS/postgresql.svg"},
+            {name : "Mongodb", logo : "/ICONS/mongodb.svg"}
 
         ]
     },
     {
         heading : "Tools and technologies",
         Skills : [
-            {name : "git", logo : "./components/assests/Logo-icons/git-seeklogo.svg"},
-            {name : "docker", logo : "./components/assests/Logo-icons/mongodb-seeklogo.svg"},
-            {name : "Nodejs", logo : "./components/assests/Logo-icons/node-js-seeklogo.svg"},
-            {name : "Postman", logo : "./components/assests/Logo-icons/postman-seeklogo.svg"}
+            {name : "git", logo : "/ICONS/git.svg"},
+            {name : "docker", logo : "/ICONS/docker.svg"},
+            {name : "Nodejs", logo : "/ICONS/nodedotjs.svg"},
+            {name : "Postman", logo : "/ICONS/postman.svg"}
         ]
     }
 ]
