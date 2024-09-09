13eabe0a8b3fc66c5294c7c3f66771317739b6e2
Rahul <rahulguptaji1019@gmail.com>
2024-09-09 15:28:20 +0530

    Added Public/Images

---
+++ public/ProjectData.ts
@@ -0,0 +1,58 @@
+
+// import Image from 'next/image';
+// import GatherHub from "./Images/Gatherhub.png"
+
+// export const ProjectData : ProjectDetails[] = [
+//     {
+//         title : "GatherHub",
+//         description : "Something about Gatherhub",
+//         image : GatherHub,
+//         icons : [GatherHub,GatherHub],
+//     }
+// ]
+
+// console.log(ProjectData);
+
+export const ProjectsData = [
+    {
+        name: 'Gatherhub',
+        desc: "GatherHub project, a community event management application. This backend provides APIs for creating, managing, and registering for events, using a variety of modern technologies.",
+        liveLink: 'https://rxhxul-gatherhub.vercel.app/login',
+        githubLink: 'https://github.com/w-rahul/gather-hub.git',
+        coverImg: '/Images/Gatherhub.png',
+        imgs: [],
+        tech: [],
+      },
+      {
+        name: 'Portfolio',
+        desc: "GatherHub",
+        githubLink: 'https://github.com/w-rahul/Rahul-Portfolio.git',
+        coverImg: '/Images/Portfolio.png',
+        imgs: [],
+        tech: [],
+      },
+      {
+        name: 'API-proxy-server',
+        desc: "This project implements an API proxy server built with Express.js. It provides several key functionalities: API Proxying, Rate Limiting and Caching",
+        githubLink: 'https://github.com/w-rahul/API-proxy-server.git',
+        coverImg: '/Images/Api-proxy.png',
+        imgs: [],
+        tech: [],
+      },{
+        name: 'EchoInk',
+        desc: "ull-stack blog platform built with modern web technologies. The backend is deployed on Cloudflare Workers using Hono.js and Accelerator for database connection pooling, with Prisma as the ORM and PostgreSQL for the database. The frontend is developed using React and Vite, styled with Tailwind CSS. Zod is used for data validation across both backend and frontend, and Wrangler is utilized for managing Cloudflare Workers.",
+        liveLink: 'https://rxhxul-blog-app.vercel.app/',
+        githubLink: 'https://github.com/w-rahul/Blog-app.gitt',
+        coverImg: '/Images/Blog.png',
+        imgs: [],
+        tech: [],
+      },
+      {
+        name: 'Payment-App',
+        desc: "This project is designed to provide a simple payment system where users can create accounts, view other users, and send money",
+        githubLink: 'https://github.com/w-rahul/Payment-app.git',
+        coverImg: '/Images/Paytm.png',
+        imgs: [],
+        tech: [],
+      },
+]
